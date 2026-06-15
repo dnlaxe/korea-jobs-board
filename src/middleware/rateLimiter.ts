a327d76d21b4eb7daacc5316ec0b339d95b66a07
dnlaxe <dnlaxe@gmail.com>
2026-06-15 15:45:35 +0900

    Switched to neon-http for vercel

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,28 +1,28 @@
-import { NextFunction, Request, Response } from "express";
-import { RateLimiterPostgres } from "rate-limiter-flexible";
-import { pool } from "../db/db.js";
+// import { NextFunction, Request, Response } from "express";
+// import { RateLimiterPostgres } from "rate-limiter-flexible";
+// import { pool } from "../db/db.js";
 
-const rateLimiter = new RateLimiterPostgres({
-  storeClient: pool,
-  keyPrefix: "middleware",
-  points: 10, // requests
-  duration: 1, // seconds
-  blockDuration: 10, // seconds
-});
+// const rateLimiter = new RateLimiterPostgres({
+//   storeClient: pool,
+//   keyPrefix: "middleware",
+//   points: 10, // requests
+//   duration: 1, // seconds
+//   blockDuration: 10, // seconds
+// });
 
-const rateLimiterMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  rateLimiter
-    .consume(req.ip ?? "unknown")
-    .then(() => {
-      next();
-    })
-    .catch(() => {
-      res.status(429).send("Too Many Requests");
-    });
-};
+// const rateLimiterMiddleware = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+// ) => {
+//   rateLimiter
+//     .consume(req.ip ?? "unknown")
+//     .then(() => {
+//       next();
+//     })
+//     .catch(() => {
+//       res.status(429).send("Too Many Requests");
+//     });
+// };
 
-export default rateLimiterMiddleware;
+// export default rateLimiterMiddleware;
